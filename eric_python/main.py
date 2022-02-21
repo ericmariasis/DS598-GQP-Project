4174f828f3a02181239598d17a6c9e042210e011
Eric Mariasis <ericmariasis829@gmail.com>
2022-02-21 17:16:26 -0500

    latest updates to main.py

--- eric_python/main.py
+++ eric_python/main.py
@@ -4,6 +4,9 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 from math import floor
+
+from keras import Sequential, optimizers, regularizers
+from keras.layers import MaxPool2D, Dropout, Conv2D, Flatten, Dense
 from termcolor import colored as cl
 from datetime import timedelta
 from datetime import datetime
@@ -13,7 +16,17 @@ from backtesting.lib import crossover, SignalStrategy, TrailingStrategy
 import config
 import json
 import talib as ta
+import tensorflow as tf
+from tensorflow import keras
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from keras import backend as K
 # Requests
+
+# Constants
+HOLD = 0
+BUY = 1
+SELL = 2
 
 def configure_full_df_print():
     pd.set_option('display.max_rows', None)
@@ -21,6 +34,140 @@ def configure_full_df_print():
     pd.set_option('display.width', None)
     pd.set_option('display.max_colwidth', None)
 
+def f1_metric(y_true, y_pred):
+    """
+    this calculates precision & recall
+    """
+
+    def recall(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))  # mistake: y_pred of 0.3 is also considered 1
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    # y_true_class = tf.math.argmax(y_true, axis=1, output_type=tf.dtypes.int32)
+    # y_pred_class = tf.math.argmax(y_pred, axis=1, output_type=tf.dtypes.int32)
+    # conf_mat = tf.math.confusion_matrix(y_true_class, y_pred_class)
+    # tf.Print(conf_mat, [conf_mat], "confusion_matrix")
+
+    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
+
+def reshape_as_image(x, img_width, img_height):
+    x_temp = np.zeros((len(x), img_height, img_width))
+    for i in range(x.shape[0]):
+        # print(type(x), type(x_temp), x.shape)
+        x_temp[i] = np.reshape(x[i], (img_height, img_width))
+
+    return x_temp
+
+def label_df(dframe, prices, windowSize, numDays):
+    winBegin = 0
+    winEnd = winBegin + windowSize
+    countRow = 0
+    labels = np.zeros([numDays])
+    while countRow <= numDays:
+        minValue = min(prices[winBegin:winEnd])
+        maxValue = max(prices[winBegin:winEnd])
+        print("MIN VALUE ISSSS", minValue)
+        print("MAX VALUE ISSSS", maxValue)
+        for i, val in enumerate(prices[winBegin:winEnd]):
+            print("I VAL ISSSSS", i)
+            finalInd = i + winBegin
+            if val == minValue and val is not None:
+                print("GOT TO DA BUY BABYYYYY")
+                labels[finalInd] = HOLD
+                labels[finalInd+1] = BUY
+            elif val == maxValue and val is not None:
+                labels[finalInd] = HOLD
+                labels[finalInd+1] = SELL
+            elif val is not None:
+                labels[finalInd] = HOLD
+        winBegin = winEnd + 1
+        winEnd = winBegin + windowSize
+        countRow = winEnd
+    dframe['y'] = labels
+    return dframe
+
+def build_model(full_data):
+    params = {'batch_size': 80,
+              'conv2d_layers': {'conv2d_do_1': 0.2, 'conv2d_filters_1': 32, 'conv2d_kernel_size_1': 3, 'conv2d_mp_1': 0,
+                                'conv2d_strides_1': 1, 'kernel_regularizer_1': 0.0, 'conv2d_do_2': 0.3,
+                                'conv2d_filters_2': 64, 'conv2d_kernel_size_2': 3, 'conv2d_mp_2': 2,
+                                'conv2d_strides_2': 1,
+                                'kernel_regularizer_2': 0.0, 'layers': 'two'},
+              'dense_layers': {'dense_do_1': 0.3, 'dense_nodes_1': 128, 'kernel_regularizer_1': 0.0, 'layers': 'one'},
+              'epochs': 3000, 'lr': 0.001, 'optimizer': 'adam'}
+    full_data_no_y = full_data.drop(columns="y")
+    X_train_full, X_test, y_train_full, y_test = train_test_split(full_data_no_y.values, full_data['y'].values, random_state=42)
+    X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
+    X_train = reshape_as_image(X_train, 3, 3)
+    X_valid = reshape_as_image(X_valid, 3, 3)
+    X_test = reshape_as_image(X_test, 3, 3)
+    X_train = np.stack((X_train,) * 3, axis=-1)
+    X_test = np.stack((X_test,) * 3, axis=-1)
+    X_valid = np.stack((X_valid,) * 3, axis=-1)
+    print("XTRAIN SHAPE IS", X_train.shape)
+    model = Sequential()
+
+    print("Training with params {}".format(params))
+
+    conv2d_layer1 = Conv2D(params["conv2d_layers"]["conv2d_filters_1"],
+                           params["conv2d_layers"]["conv2d_kernel_size_1"],
+                           strides=params["conv2d_layers"]["conv2d_strides_1"],
+                           kernel_regularizer=regularizers.l2(params["conv2d_layers"]["kernel_regularizer_1"]),
+                           padding='same', activation="relu", use_bias=True,
+                           kernel_initializer='glorot_uniform',
+                           input_shape=(X_train[0].shape[0],
+                                        X_train[0].shape[1], X_train[0].shape[2]))
+    model.add(conv2d_layer1)
+    if params["conv2d_layers"]['conv2d_mp_1'] > 1:
+        model.add(MaxPool2D(pool_size=params["conv2d_layers"]['conv2d_mp_1']))
+
+    model.add(Dropout(params['conv2d_layers']['conv2d_do_1']))
+    if params["conv2d_layers"]['layers'] == 'two':
+        conv2d_layer2 = Conv2D(params["conv2d_layers"]["conv2d_filters_2"],
+                               params["conv2d_layers"]["conv2d_kernel_size_2"],
+                               strides=params["conv2d_layers"]["conv2d_strides_2"],
+                               kernel_regularizer=regularizers.l2(params["conv2d_layers"]["kernel_regularizer_2"]),
+                               padding='same', activation="relu", use_bias=True,
+                               kernel_initializer='glorot_uniform')
+        model.add(conv2d_layer2)
+
+        if params["conv2d_layers"]['conv2d_mp_2'] > 1:
+            model.add(MaxPool2D(pool_size=params["conv2d_layers"]['conv2d_mp_2']))
+
+        model.add(Dropout(params['conv2d_layers']['conv2d_do_2']))
+
+    model.add(Flatten())
+
+    model.add(Dense(params['dense_layers']["dense_nodes_1"], activation='relu'))
+    model.add(Dropout(params['dense_layers']['dense_do_1']))
+
+    if params['dense_layers']["layers"] == 'two':
+        model.add(Dense(params['dense_layers']["dense_nodes_2"], activation='relu',
+                        kernel_regularizer=params['dense_layers']["kernel_regularizer_1"]))
+        model.add(Dropout(params['dense_layers']['dense_do_2']))
+
+    model.add(Dense(3, activation='softmax'))
+
+    if params["optimizer"] == 'rmsprop':
+        optimizer = optimizers.RMSprop(lr=params["lr"])
+    elif params["optimizer"] == 'sgd':
+        optimizer = optimizers.SGD(lr=params["lr"], decay=1e-6, momentum=0.9, nesterov=True)
+    elif params["optimizer"] == 'adam':
+        optimizer = tf.optimizers.Adam(learning_rate=params["lr"], beta_1=0.9, beta_2=0.999, amsgrad=False)
+
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy', f1_metric])
+
+    return model, X_train, X_valid, X_test, X_train, y_train, y_valid
 base_url = 'https://cloud.iexapis.com/'
 version = 'stable/'
 
@@ -46,16 +193,16 @@ print(f'API Call: {api_call}')
 #print(f'Headers: {r.headers}') # Show headers
 #print(f"IEX Cloud Credits Used: {r.headers['iexcloud-credits-used']}")
 #print(f'Data: {data}') # Print decodedJSON object
-data = open("data_regular_by_minute.json", "r")
+data = open("data_regular_max.json", "r")
 a_dictionary = json.load(data)
 #print(json.dumps(a_dictionary, sort_keys=True, indent=4))
 
 
 data_pd = pd.json_normalize(a_dictionary)
 print(data_pd)
-data_pd['date'] = data_pd['date'] + ' ' + data_pd['minute']
+#data_pd['date'] = data_pd['date'] + ' ' + data_pd['minute']
 data_pd['date'] = pd.to_datetime(data_pd['date'])
-data_pd.drop('minute', axis=1, inplace=True)
+#data_pd.drop('minute', axis=1, inplace=True)
 data_pd.set_index('date', inplace=True)
 print(data_pd.index)
 print(data_pd)
@@ -68,6 +215,32 @@ df['Close'] = df['close']
 df['Volume'] = df['volume']
 
 df = df.dropna()
+
+"""
+Assemble a dataframe of technical indicator series for a single stock
+"""
+
+df["adx"] = ta.ADX(df['high'],df['low'],df['close'])
+df["rsi"] = ta.RSI(df['close'])
+df["willr"] = ta.WILLR(df['high'],df['low'],df['close'])
+df['mfi'] = ta.MFI(df['high'], df['low'], df['close'], df['volume'])
+configure_full_df_print()
+X = df[['Open', 'High', 'Low', 'Close', 'Volume', 'adx', 'rsi', 'willr', 'mfi']].copy()
+x = X.values #returns a numpy array
+min_max_scaler = preprocessing.MinMaxScaler()
+x_scaled = min_max_scaler.fit_transform(x)
+X = pd.DataFrame(x_scaled, columns=X.columns)
+print("DOSE X COLUMNSSSSSS!!!!!")
+print(X.columns)
+print(X)
+X = label_df(X, X['Close'].values, 11, X.shape[0])
+print(X.columns)
+print(X)
+model, X_train, X_valid, X_test, X_train, y_train, y_valid = build_model(X)
+print("XTEST SHAPE", X_valid.shape)
+y_pred = model.predict(X_test)
+print(y_pred)
+exit()
 
 class ParabolicStrategy(Strategy):
 
