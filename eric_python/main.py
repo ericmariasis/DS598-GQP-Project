f309d11d764117b273aecb636a58c3670e95b9a4
ericmariasis <ericmariasis829@gmail.com>
2022-04-04 12:31:03 -0400

    added boilerplate integration class

--- eric_python/main.py
+++ eric_python/main.py
@@ -9,6 +9,7 @@ from math import floor
 
 from keras import Sequential, optimizers, regularizers
 from keras.layers import MaxPool2D, Dropout, Conv2D, Flatten, Dense
+from sklearn.metrics import accuracy_score
 from termcolor import colored as cl
 from datetime import timedelta
 from datetime import datetime
@@ -22,6 +23,9 @@ from tensorflow import keras
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from keras import backend as K
+from sklearn.model_selection import TimeSeriesSplit
+import multiprocessing
+from multiprocessing import Process
 # Requests
 
 # Constants
@@ -104,10 +108,19 @@ def build_model(full_data):
                                 'conv2d_strides_2': 1,
                                 'kernel_regularizer_2': 0.0, 'layers': 'two'},
               'dense_layers': {'dense_do_1': 0.3, 'dense_nodes_1': 128, 'kernel_regularizer_1': 0.0, 'layers': 'one'},
-              'epochs': 3000, 'lr': 0.05, 'optimizer': 'adam'}
+              'epochs': 30000, 'lr': 0.05, 'optimizer': 'adam'}
     full_data_no_y = full_data.drop(columns="y")
-    X_train_full, X_test, y_train_full, y_test = train_test_split(full_data_no_y.values, full_data['y'].values, random_state=42)
-    X_train, X_valid, y_train, y_valid = train_test_split(X_train_full, y_train_full, random_state=42)
+
+    tscv = TimeSeriesSplit(n_splits=9)
+    for tr_index, test_index in tscv.split(full_data_no_y.values):
+        X_train_full, X_test = full_data_no_y.values[tr_index], full_data_no_y.values[test_index]
+        y_train_full, y_test = full_data['y'].values[tr_index], full_data['y'].values[test_index]
+
+    for tr_index, val_index in tscv.split(X_train_full):
+        X_train, X_valid = X_train_full[tr_index], X_train_full[val_index]
+        y_train, y_valid = y_train_full[tr_index], y_train_full[val_index]
+
+
     print("model X_train shape", X_train.shape)
     X_train = reshape_as_image(X_train, 3, 3)
     X_valid = reshape_as_image(X_valid, 3, 3)
@@ -260,6 +273,8 @@ df['predicted_y'] = np.argmax(y_pred_full, axis=1)
 #print(df['y'])
 
 print(df[['y','predicted_y']])
+print("NUM Ys IS", len(df['y']))
+print("ACCURACY SCORE", accuracy_score(df['y'], df['predicted_y']))
 
 use_ml = True
 class ParabolicStrategy(Strategy):
@@ -289,12 +304,12 @@ class ParabolicStrategy(Strategy):
             #if use_ml and self.pred_y[0] == BUY:
                 #print("ML BUY!!!!!")
             #print("NEXT PREDICTED ACTION", self.pred_y[0])
-            self.buy(size=0.2, tp=350, limit=100, sl=90)
+            self.buy(size=0.2, tp=350, limit=100)
         if ((self.adx[-1] > 25 and self.adx[-2] <= 25 and self.mdi[-1] > self.pdi[-1] and self.rsi[-1] < 30) or (use_ml and self.pred_y[0] == SELL)) and not self.position.is_short:
             #if use_ml and self.pred_y[0] == SELL:
                 #print("ML SELL!!!!!")
             #print("NEXT PREDICTED ACTION", self.pred_y[0])
-            self.sell(size=0.2, tp=200, limit=300, sl=400)
+            self.sell(size=0.2, tp=200, limit=300)
         # close buy position when price moves below PSAR and close sell position when price moves below psar
         if self.position.is_long and self.sar[-1] > self.close[-1] and self.sar[-2] <= self.close[-2]:
             #print("NEXT PREDICTED ACTION", self.pred_y[0])
@@ -319,174 +334,83 @@ def expectunity(stats):
 
 print(expectunity(stats))
 
-#MISCELLANEOUS
-# print(len(data_pd['chart'][0]))
-# print(len(data_pd['indicator'][0][0]))
-#
-# # DIRECTIONAL INDEXES
-# data_dis = open("data_di.json", "r")
-# a_dictionary_dis = json.load(data_dis)
-# data_pd_dis = pd.json_normalize(a_dictionary_dis)
-# print("DATAPDDIS", len(data_pd_dis['indicator'][0][1]))
-#
-# df = pandas.DataFrame(columns=['adx','plus_di','minus_di'])
-# # print(data_pd['chart'][0][0])
-# # df_dict = pd.DataFrame(pd.Series([list(data_pd['chart'][0][0].items())]))
-# # print(df_dict)
-# print("FIRSTLEN IS", len(data_pd['indicator'][0][0]))
-# print("SECONDLEN IS", len(data_pd_dis['indicator'][0][0]))
-#
-# for key in data_pd['chart'][0][0]:
-#     print(key)
-#     df[key] = pd.NaT
-# #
-# for ind, item in enumerate(data_pd['chart'][0]):
-#     adx = []
-#     adx.append(data_pd['indicator'][0][0][ind])
-#     print("PLUSDI IS", data_pd_dis['indicator'][0][0][ind])
-#     print("MINUSDI IS", data_pd_dis['indicator'][0][1][ind])
-#     adx.append(data_pd_dis['indicator'][0][0][ind])
-#     adx.append(data_pd_dis['indicator'][0][1][ind])
-#     #print("ADX IS", adx)
-#     #print("IND IS", ind)
-#     values = []
-#     for c in df.columns:
-#         print("C IS", c)
-#         if c != 'adx' and c != 'plus_di' and c!= 'minus_di':
-#             values.append(data_pd['chart'][0][ind].get(c))
-#             #print("VALUES IS", values)
-#     adx.extend(values)
-#     print(len(adx))
-#     print(len(df.columns))
-#     df.loc[len(df)] = adx
-#
-# configure_full_df_print()
-# df.dropna(inplace=True)
-# print(df)
-# df2 = df[['adx','plus_di','minus_di','date','open','high','low','close','volume']]
-# print(df2)
-# aapl = df2
-#
-# ax1 = plt.subplot2grid((11,1), (0,0), rowspan = 5, colspan = 1)
-# ax2 = plt.subplot2grid((11,1), (6,0), rowspan = 5, colspan = 1)
-# ax1.plot(aapl['close'], linewidth = 2, color = '#ff9800')
-# ax1.set_title('AAPL CLOSING PRICE')
-# ax2.plot(aapl['plus_di'], color = '#26a69a', label = '+ DI 14', linewidth = 3, alpha = 0.3)
-# ax2.plot(aapl['minus_di'], color = '#f44336', label = '- DI 14', linewidth = 3, alpha = 0.3)
-# ax2.plot(aapl['adx'], color = '#2196f3', label = 'ADX 14', linewidth = 3)
-# ax2.axhline(25, color = 'grey', linewidth = 2, linestyle = '--')
-# ax2.legend()
-# ax2.set_title('AAPL ADX 14')
-# plt.show()
-#
-#
-# def implement_adx_strategy(prices, pdi, ndi, adx):
-#     buy_price = []
-#     sell_price = []
-#     adx_signal = []
-#     signal = 0
-#
-#     for i in range(len(prices)):
-#         if adx.iloc[i - 1] < 25 and adx.iloc[i] > 25 and pdi.iloc[i] > ndi.iloc[i]:
-#             if signal != 1:
-#                 buy_price.append(prices.iloc[i])
-#                 sell_price.append(np.nan)
-#                 signal = 1
-#                 adx_signal.append(signal)
-#             else:
-#                 buy_price.append(np.nan)
-#                 sell_price.append(np.nan)
-#                 adx_signal.append(0)
-#         elif adx.iloc[i - 1] < 25 and adx.iloc[i] > 25 and ndi.iloc[i] > pdi.iloc[i]:
-#             if signal != -1:
-#                 buy_price.append(np.nan)
-#                 sell_price.append(prices.iloc[i])
-#                 signal = -1
-#                 adx_signal.append(signal)
-#             else:
-#                 buy_price.append(np.nan)
-#                 sell_price.append(np.nan)
-#                 adx_signal.append(0)
-#         else:
-#             buy_price.append(np.nan)
-#             sell_price.append(np.nan)
-#             adx_signal.append(0)
-#
-#     return buy_price, sell_price, adx_signal
-#
-#
-# buy_price, sell_price, adx_signal = implement_adx_strategy(aapl['close'], aapl['plus_di'], aapl['minus_di'], aapl['adx'])
-#
-# ax1 = plt.subplot2grid((11,1), (0,0), rowspan = 5, colspan = 1)
-# ax2 = plt.subplot2grid((11,1), (6,0), rowspan = 5, colspan = 1)
-# ax1.plot(aapl['close'], linewidth = 3, color = '#ff9800', alpha = 0.6)
-# ax1.set_title('AAPL CLOSING PRICE')
-# print(aapl.index)
-# ax1.plot(aapl.index, buy_price, marker = '^', color = '#26a69a', markersize = 14, linewidth = 0, label = 'BUY SIGNAL')
-# ax1.plot(aapl.index, sell_price, marker = 'v', color = '#f44336', markersize = 14, linewidth = 0, label = 'SELL SIGNAL')
-# ax2.plot(aapl['plus_di'], color = '#26a69a', label = '+ DI 14', linewidth = 3, alpha = 0.3)
-# ax2.plot(aapl['minus_di'], color = '#f44336', label = '- DI 14', linewidth = 3, alpha = 0.3)
-# ax2.plot(aapl['adx'], color = '#2196f3', label = 'ADX 14', linewidth = 3)
-# ax2.axhline(25, color = 'grey', linewidth = 2, linestyle = '--')
-# ax2.legend()
-# ax2.set_title('AAPL ADX 14')
-# plt.show()
-#
-# # HOLD POSITION
-# position = []
-# for i in range(len(adx_signal)):
-#     if adx_signal[i] > 1:
-#         position.append(0)
-#     else:
-#         position.append(1)
-#
-# for i in range(len(aapl['close'])):
-#     if adx_signal[i] == 1:
-#         position[i] = 1
-#     elif adx_signal[i] == -1:
-#         position[i] = 0
-#     else:
-#         position[i] = position[i - 1]
-#
-# close_price = aapl['close']
-# plus_di = aapl['plus_di']
-# minus_di = aapl['minus_di']
-# adx = aapl['adx']
-# adx_signal = pd.DataFrame(adx_signal).rename(columns={0: 'adx_signal'}).set_index(aapl.index)
-# position = pd.DataFrame(position).rename(columns={0: 'adx_position'}).set_index(aapl.index)
-#
-# frames = [close_price, plus_di, minus_di, adx, adx_signal, position]
-# strategy = pd.concat(frames, join='inner', axis=1)
-#
-# print(strategy)
-#
-# ## BACKTEST
-# aapl_ret = pd.DataFrame(np.diff(aapl['close'])).rename(columns={0: 'returns'})
-# adx_strategy_ret = []
-#
-# for i in range(len(aapl_ret)):
-#     returns = aapl_ret['returns'].iloc[i] * strategy['adx_position'].iloc[i]
-#     adx_strategy_ret.append(returns)
-#
-# adx_strategy_ret_df = pd.DataFrame(adx_strategy_ret).rename(columns={0: 'adx_returns'})
-# investment_value = 100000
-# number_of_stocks = floor(investment_value / aapl['close'].iloc[-1])
-# adx_investment_ret = []
-#
-# for i in range(len(adx_strategy_ret_df['adx_returns'])):
-#     returns = number_of_stocks * adx_strategy_ret_df['adx_returns'][i]
-#     adx_investment_ret.append(returns)
-#
-# adx_investment_ret_df = pd.DataFrame(adx_investment_ret).rename(columns={0: 'investment_returns'})
-# total_investment_ret = round(sum(adx_investment_ret_df['investment_returns']), 2)
-# profit_percentage = floor((total_investment_ret / investment_value) * 100)
-# print(cl('Profit gained from the ADX strategy by investing $100k in AAPL : {}'.format(total_investment_ret),
-#          attrs=['bold']))
-# print(cl('Profit percentage of the ADX strategy : {}%'.format(profit_percentage), attrs=['bold']))
+class DummyParabolicStrategy(Strategy):
+
+    def init(self):
+        # In init() and in next() it is important to call the
+        # super method to properly initialize the parent classes
+        super().init()
+        self.high = self.data.High
+        self.low = self.data.Low
+        self.close = self.data.close
+        #print("---------------CLOSE---------")
+        #print(self.close)
+        self.adx = self.I(ta.ADX, self.high, self.low, self.close)
+        self.sar = self.I(ta.SAR, self.high, self.low)
+        #print("----------SAR-------------")
+        #print(self.sar)
+        self.pdi = self.I(ta.PLUS_DI, self.high, self.low, self.close)
+        self.mdi = self.I(ta.MINUS_DI, self.high, self.low, self.close)
+        self.rsi = self.I(ta.RSI, self.close)
+        self.pred_y = self.data.predicted_y
+
+    def next(self):
+        #print("SELF ADX -2 -1 0 [", self.adx[-2], self.adx[-1], self.adx[0], "]")
+        #print("NEXT PREDICTED ACTION GENERAL", self.pred_y[0])
+        if ((self.adx[-1] > 25 and self.adx[-2] <= 25 and self.pdi[-1] > self.mdi[-1]) or (use_ml and self.pred_y[0] == BUY)) and not self.position.is_long:
+            #if use_ml and self.pred_y[0] == BUY:
+                #print("ML BUY!!!!!")
+            #print("NEXT PREDICTED ACTION", self.pred_y[0])
+            self.buy(size=0.2, tp=350, limit=100)
+        if ((self.adx[-1] > 25 and self.adx[-2] <= 25 and self.mdi[-1] > self.pdi[-1] and self.rsi[-1] < 30) or (use_ml and self.pred_y[0] == SELL)) and not self.position.is_short:
+            #if use_ml and self.pred_y[0] == SELL:
+                #print("ML SELL!!!!!")
+            #print("NEXT PREDICTED ACTION", self.pred_y[0])
+            self.sell(size=0.2, tp=200, limit=300)
+        # close buy position when price moves below PSAR and close sell position when price moves below psar
+        if self.position.is_long and self.sar[-1] > self.close[-1] and self.sar[-2] <= self.close[-2]:
+            #print("NEXT PREDICTED ACTION", self.pred_y[0])
+            self.position.close()
+        if self.position.is_short and self.sar[-1] < self.close[-1] and self.sar[-2] >= self.close[-2]:
+            #print("NEXT PREDICTED ACTION", self.pred_y[0])
+            self.position.close()
+
+def runInParallel(*fns):
+  proc = []
+  for fn in fns:
+    p = Process(target=fn)
+    p.start()
+    proc.append(p)
+  for p in proc:
+    p.join()
+
+class IntegrationClass:
+    cash = 0
+    commission = 0
+    # default constructor
+    def __init__(self, cash, commission):
+        self.cash = cash
+        self.commission = commission
+    def backtestObj(self, df, strat, cash, commission):
+        return Backtest(df, strat, cash=cash, commission=commission)
+    def statsForBacktestObj(self, obj):
+        return obj.run()
+    def run(self):
+        btOne = self.backtestObj(df, DummyParabolicStrategy, self.cash/2, self.commission)
+        btTwo = self.backtestObj(df, ParabolicStrategy, self.cash/2, self.commission)
+
+        pool = multiprocessing.Pool(processes=3)
+        args = [btOne, btTwo]
+        statsresults = pool.map(self.statsForBacktestObj, args)
+        print("STATS RESULTS IS ", statsresults)
+        print("STATS LEN IS", len(statsresults))
+        #statsOne = self.statsForBacktestObj(btOne)
+        #statsTwo = self.statsForBacktestObj(btTwo)
 
 
-#a_file = open("data.json", "w")
-#json.dump(data, a_file)
+#f = open("DS598 Code in Python.R", "r")
+#print(f.read())
 
-# Fetch marketcap from first endpont_path (no field_param used)
-#print(f'Market Cap: {data["marketcap"]}')
+if __name__ == '__main__':
+    integration_obj = IntegrationClass(cash=1000000, commission=0.02)
+    integration_obj.run()
